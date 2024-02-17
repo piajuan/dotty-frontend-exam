d1ea65365269d2d15890ac51985d6479cafd1c6e
Pia Juan <piajuan.dev@gmail.com>
2024-02-17 20:39:59 +0800

    added section pagination

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ const functions = (function () {
 
     // Slideshow tabs
     const tabs = function () {
-        // Show the first tab and hide the rest
         $('#tabs-nav .tabs__item:first-child .tabs__link').addClass('active');
         $('.tabs__content').hide();
         $('.tabs__content:first').show();
 
-        // handle click
         $('#tabs-nav .tabs__link').click(function(){
             $('#tabs-nav .tabs__link').removeClass('active');
             $(this).addClass('active');
@@ -21,8 +19,34 @@ const functions = (function () {
         });
     };
 
+    // inViewport 
+    $.fn.isInViewport = function() {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight() / 2;
+        var viewportTop = $(window).scrollTop();
+        var viewportHalf = viewportTop + $(window).height() / 2;
+        return elementBottom > viewportTop && elementTop < viewportHalf;
+    };
+
+    // Section pagination
+    const sectionPagination = function() {
+        $(window).on('load resize scroll', function() {
+            $('#section-pagination .pagination__bullet').each(function() {
+                var href = $(this).attr('href');
+                var target = $(href);
+        
+                if (target.isInViewport()) {
+                    $('#section-pagination').fadeIn();
+                    $('#section-pagination .pagination__bullet').removeClass('active');
+                    $(this).addClass('active');
+                } 
+            });
+        });
+    }
+
     function initialize() {
         tabs();
+        sectionPagination();
     }
 
     return {
